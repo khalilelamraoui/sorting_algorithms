a2220703e3b1f3ec88c9d788ef0d7aa2b72ac345
khalilelamraoui <khalilelam16@gmail.com>
2024-01-19 20:50:29 +0100

    fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,31 +8,31 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next, *prev;
+	listint_t *current, *next, *prev;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        next = current->next;
-        prev = current->prev;
-        while (prev && prev->n > current->n)
-        {
-            prev->next = current->next;
-            if (current->next)
-                current->next->prev = prev;
-            current->next = prev;
-            current->prev = prev->prev;
-            if (prev->prev)
-                prev->prev->next = current;
-            prev->prev = current;
-            prev = current->prev;
-            if (!current->prev)
-                *list = current;
-            print_list(*list);
-        }
-        current = next;
-    }
+	current = (*list)->next;
+	while (current)
+	{
+		next = current->next;
+		prev = current->prev;
+		while (prev && prev->n > current->n)
+		{
+			prev->next = current->next;
+			if (current->next)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			if (prev->prev)
+				prev->prev->next = current;
+			prev->prev = current;
+			prev = current->prev;
+			if (!current->prev)
+				*list = current;
+			print_list(*list);
+		}
+		current = next;
+	}
 }
